a1874e078bee77ec8ef5299ce0ba105f1cc52ad5
GuilhermeFerreira08 <52543533+GuilhermeFerreira08@users.noreply.github.com>
2021-04-23 19:17:40 -0300

    Update urls.py
    novas urls

--- projeto/mysite/polls/urls.py
+++ projeto/mysite/polls/urls.py
@@ -5,4 +5,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('',views.index,name='index'),
+    path('polls/usuario',views.ClienteCreate.as_view(),name='usuario'),
+    path('polls/usuario/endereco',views.EnderecoDetailView.as_view(),name='endereco'),
+    path('projeto/mysite/index/',views.IndexProdutos.as_view(),name='index'),
+    path('projeto/mysite/index/sessao/<id>',views.DetailCliente.as_view(),name='sessao_cliente'),
+    #path('projeto/mysite/categoria/',view.CategoriaListView,name = 'categorias'),
+    #path('projeto/mysite/categoria/<nome>/',views.CategoriaDetailView, name = 'categoria'),
+    #path('projeto/mysite/categoria/<nome>/produto/',views.IndexProdutos.asview(), name='exibe_produtos'),
+    #path('projeto/mysite/categoria/<nome>/produto/<id>',views.DetailView.as_view(), name='detalhe_produto'),
+    #path('projeto/mysite/cliente/<id>/',views.ClienteCreate.cadastro().as_view(), name='cadastro'),
+    path('projeto/mysite/cliente/<id>/atualiza',views.EnderecoUpdate.as_view(),name = 'atualiza_endereco'),
+    #path('projeto/mysite/cliente/<id>/',views.EnderecoDelete.as_view(),name = 'deleta_endereco'),
+    #path('projeto/mysite/cliente/<id>/compras/',views,name = 'carrinho'),
+    #path('projeto/mysite/categoria/<nome>/produto/<id>', views.DeleteItem.as_view()),name='deleta_compra'),
     ]
